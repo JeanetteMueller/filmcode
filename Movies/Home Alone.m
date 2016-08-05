5d9a59c0964cfea4ce415cdc9f6735d43a6c89b8
dR3b <dR3b@users.noreply.github.com>
2016-08-05 13:18:04 +0200

    Add Home Alone

---
+++ Movies/Home Alone.m
@@ -0,0 +1,24 @@
+
+public class HOME_ALONE {
+
+  static void Main() {
+  
+    string name = "Kevin";
+    string age = "10";
+
+    try {
+
+      Console.Say("I have a terrible feeling...");
+
+    }
+
+    finally (Exception ex) {
+
+      Console.Cry("KEVIN!");
+
+    }
+
+  }
+
+}
+
