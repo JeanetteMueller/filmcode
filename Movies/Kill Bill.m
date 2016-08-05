1879ee1c6760f95125501b548a805dac85d4a3d7
dR3b <dR3b@users.noreply.github.com>
2016-08-05 14:28:49 +0200

    Add Kill Bill

---
+++ Movies/Kill Bill.m
@@ -0,0 +1,17 @@
+
+import KillBill
+
+def main():
+
+  Deadly_Viper_Assassination_Squad = [ "Black Mamba",
+    "Cottonmouth",
+    "Copperhead",
+    "California Mountain Snake",
+    "Sidewinder" ]
+  
+  for Assassin in Deadly_Viper_Assassination_Squad:
+    kill(assassin)
+
+if __name__ == "__main__":
+    main()
+
